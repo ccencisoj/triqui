e8b9515602d5f54120448e4a87a0c0eaf2741b3a
cristian <bizapro123@gmail.com>
2022-06-18 22:41:38 -0500

    Se actualizaron las variables de entorno

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const production = true;
 
 module.exports = {
   env: production ? {
-    SOCKET_URL: "https://18.228.212.106:442"
+    SOCKET_URL: "https://18.228.212.106:443"
   } : {
     SOCKET_URL: "http://192.168.100.6:4000"
   }
